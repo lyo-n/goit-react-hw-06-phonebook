5ce43d6b0e4d389df8f16ba44b106a0b458ef56d
lyo-n <1y0mmmm@gmail.com>
2021-01-27 23:43:13 +0200

    Updates

---
+++ precache-manifest.103508275b379987b18af3a6947373fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d0c2cbe16e7c28f6a34664ee22fd261",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "bfeffd907266dfeedef0",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.35df1833.chunk.css"
+  },
+  {
+    "revision": "5105a130a43e772c41e2",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.70167d38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.70167d38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfeffd907266dfeedef0",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.93bde6e0.chunk.js"
+  },
+  {
+    "revision": "fd44328cb7a21a2e0a73",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.b930395d.js"
+  }
+]);
